876c7f268451a51e46ad90cdaf719e5df80d3000
Mathijs de Langen <langen@robot-rose.nl>
2015-04-14 17:11:38 +0200

    created initial goal_pose for driving position

--- rose_operations/src/arm_driving_position/arm_driving_position.cpp
+++ rose_operations/src/arm_driving_position/arm_driving_position.cpp
@@ -2,12 +2,12 @@
 * Copyright: Rose B.V. (2014)
 *
 * Revision History:
-*	Author: Mathijs de Langen
-*	Date  : 2014/04/10
-* 		- File created.
+*   Author: Mathijs de Langen
+*   Date  : 2014/04/10
+*       - File created.
 *
 * Description:
-*	description
+*   description
 * 
 ***********************************************************************************/
 #include "arm_driving_position/arm_driving_position.hpp"
@@ -15,7 +15,22 @@
 ArmDrivingPosition::ArmDrivingPosition( std::string name, ros::NodeHandle n )
     : ArmPoseBaseClass (name, n)
 {
+    std::string                arm_name; 
+    geometry_msgs::PoseStamped goal_pose;
 
+    arm_name                    = "mico";
+    goal_pose.header.stamp      = ros::Time::now();
+    goal_pose.header.frame_id   = arm_name;
+    goal_pose.pose.position.x   = 0.0;
+    goal_pose.pose.position.y   = 0.0;
+    goal_pose.pose.position.z   = 0.0;
+
+    double roll  = 0.0;
+    double pitch = 0.0;
+    double yaw   = 0.0;
+    goal_pose.pose.orientation = rose_conversions::RPYToQuaterion(roll, pitch, yaw);
+
+    setCartesianGoal(arm_name, goal_pose);
 }
 
 ArmDrivingPosition::~ArmDrivingPosition()
