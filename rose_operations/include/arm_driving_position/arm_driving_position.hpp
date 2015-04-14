3001b4ce02eb89823053ec536145fa78f8547831
Mathijs de Langen <langen@robot-rose.nl>
2015-04-14 17:04:35 +0200

    Created arm_pose_base class, for sending cartesian positions to the arm

--- rose_operations/include/arm_driving_position/arm_driving_position.hpp
+++ rose_operations/include/arm_driving_position/arm_driving_position.hpp
@@ -15,26 +15,13 @@
 
 #include <string>
 
-#include "arm_controller_helper.hpp"
-#include "operation_base_class/operation_base_class.hpp"
+#include "arm_pose_base_class/arm_pose_base.hpp"
 
-class ArmDrivingPosition : public OperationBaseClass
+class ArmDrivingPosition : public ArmPoseBaseClass
 {
   public:
     ArmDrivingPosition(std::string name, ros::NodeHandle n);
     ~ArmDrivingPosition();
-
-  private:
-    void CB_goalReceived(const rose_operations::basic_operationGoalConstPtr& goal, SMC* smc);
-    void CB_armActionSuccess(const actionlib::SimpleClientGoalState& state,
-                             const arm_controller::manipulateResultConstPtr& result);
-    void CB_armActionFail(const actionlib::SimpleClientGoalState& state,
-                          const arm_controller::manipulateResultConstPtr& result);
-
-    bool armAction(const ArmController::Arms& arm, const arm_controller::manipulateGoal& goal);
-    void moveArmToDrivingPosition();
-    bool drivingPosition(const ArmController::Arms& arm);
-    bool closeGripper(const ArmController::Arms& arm);
 };
 
 #endif  // ARM_DRIVING_POSITION_HPP
--- rose_operations/include/arm_grabbing_position/arm_grabbing_position.hpp
+++ rose_operations/include/arm_grabbing_position/arm_grabbing_position.hpp
@@ -15,26 +15,13 @@
 
 #include <string>
 
-#include "arm_controller_helper.hpp"
-#include "operation_base_class/operation_base_class.hpp"
+#include "arm_pose_base_class/arm_pose_base.hpp"
 
-class ArmGrabbingPosition : public OperationBaseClass
+class ArmGrabbingPosition : public ArmPoseBaseClass
 {
   public:
     ArmGrabbingPosition(std::string name, ros::NodeHandle n);
     ~ArmGrabbingPosition();
-
-  private:
-    void CB_goalReceived(const rose_operations::basic_operationGoalConstPtr& goal, SMC* smc);
-    void CB_armActionSuccess(const actionlib::SimpleClientGoalState& state,
-                             const arm_controller::manipulateResultConstPtr& result);
-    void CB_armActionFail(const actionlib::SimpleClientGoalState& state,
-                          const arm_controller::manipulateResultConstPtr& result);
-
-    bool armAction(const ArmController::Arms& arm, const arm_controller::manipulateGoal& goal);
-    void moveArmToGrabbingPosition();
-    bool grabbingPosition(const ArmController::Arms& arm);
-    bool closeGripper(const ArmController::Arms& arm);
 };
 
 #endif  // ARM_GRABBING_POSITION_HPP
