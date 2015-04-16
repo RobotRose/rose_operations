98018e3204dedf2adb1876d46dab2d52f2cee5fb
Mathijs de Langen <langen@robot-rose.nl>
2015-04-16 21:11:20 +0200

    tabs to spaces

--- rose_operations/include/operation_base_class/operation_base_class.hpp
+++ rose_operations/include/operation_base_class/operation_base_class.hpp
@@ -2,12 +2,12 @@
 * Copyright: Rose B.V. (2014)
 *
 * Revision History:
-*	Author: Mathijs de Langen
-*	Date  : 2014/02/14
-* 		- File created.
+*   Author: Mathijs de Langen
+*   Date  : 2014/02/14
+*       - File created.
 *
 * Description:
-*	description
+*   description
 * 
 ***********************************************************************************/
 #ifndef OPERATION_BASE_CLASS_HPP
@@ -72,50 +72,50 @@
 class OperationBaseClass
 {
   public:
-  	typedef rose_parameter_manager::parameterAction ParameterAction;
-  	typedef boost::function< void (const std::string item_id, const std::vector<std::string> parameter_ids) > ParameterSucces;
+    typedef rose_parameter_manager::parameterAction ParameterAction;
+    typedef boost::function< void (const std::string item_id, const std::vector<std::string> parameter_ids) > ParameterSucces;
 
-	OperationBaseClass( std::string name, ros::NodeHandle n );
-	~OperationBaseClass();
+    OperationBaseClass( std::string name, ros::NodeHandle n );
+    ~OperationBaseClass();
 
   protected:
     typedef ServerMultipleClient<rose_operations::basic_operationAction> SMC;
 
-	virtual void CB_serverCancel( SMC* smc );
-	virtual void CB_goalReceived( const rose_operations::basic_operationGoalConstPtr& goal, SMC* smc );
-	
-	virtual void startOperation();
-	virtual void sendResult( bool succes );
+    virtual void CB_serverCancel( SMC* smc );
+    virtual void CB_goalReceived( const rose_operations::basic_operationGoalConstPtr& goal, SMC* smc );
+    
+    virtual void startOperation();
+    virtual void sendResult( bool succes );
     virtual void sendResult( bool succes,  std::string message );
 
-	virtual void getParameter( std::string item_id, PARAMETER_REQUEST parameter, ParameterSucces parameter_succes);
-	virtual void CB_getParameterSuccess( const actionlib::SimpleClientGoalState& state, const rose_parameter_manager::parameterResultConstPtr& result );
-	virtual void CB_getParameterFail( const actionlib::SimpleClientGoalState& state, const rose_parameter_manager::parameterResultConstPtr& result );
-	virtual void CB_getParameterActive();
-	virtual void CB_getParameterFeedback( const rose_parameter_manager::parameterFeedbackConstPtr& feedback );
+    virtual void getParameter( std::string item_id, PARAMETER_REQUEST parameter, ParameterSucces parameter_succes);
+    virtual void CB_getParameterSuccess( const actionlib::SimpleClientGoalState& state, const rose_parameter_manager::parameterResultConstPtr& result );
+    virtual void CB_getParameterFail( const actionlib::SimpleClientGoalState& state, const rose_parameter_manager::parameterResultConstPtr& result );
+    virtual void CB_getParameterActive();
+    virtual void CB_getParameterFeedback( const rose_parameter_manager::parameterFeedbackConstPtr& feedback );
 
-	virtual void executeItemAction( const std::string id, const std::vector<std::string> parameters = std::vector<std::string>() );
+    virtual void executeItemAction( const std::string id, const std::vector<std::string> parameters = std::vector<std::string>() );
     virtual void executeMultiItemAction( const std::vector<std::string> item_ids, const std::vector<std::string> parameters = std::vector<std::string>() );
     virtual void executePersonAction( const std::string id, const std::vector<std::string> parameters = std::vector<std::string>() );
     virtual void executeWaypointAction( const std::string id, const std::vector<std::string> parameters = std::vector<std::string>() );
 
     void addParameterManagerClient();
 
-	std::string 						name_;
-	ros::NodeHandle 					n_;
+    std::string                         name_;
+    ros::NodeHandle                     n_;
 
-	DatamanagerAPI*						datamanager_;
-	SMC*								smc_;
+    DatamanagerAPI*                     datamanager_;
+    SMC*                                smc_;
     OperatorMessaging*                  operator_gui_;
 
-	ParameterSucces 					parameter_succes_;
-	
-	rose_operations::basic_operationResult 	result_;
-	std::string 							current_item_id_;
-    std::vector<std::string>				current_item_ids_;
-	std::vector<std::string>				current_parameters_ids_;
-	
-	tf::TransformListener 				tf_;
+    ParameterSucces                     parameter_succes_;
+    
+    rose_operations::basic_operationResult  result_;
+    std::string                             current_item_id_;
+    std::vector<std::string>                current_item_ids_;
+    std::vector<std::string>                current_parameters_ids_;
+    
+    tf::TransformListener               tf_;
 };
 
 #endif //OPERATION_BASE_CLASS_HPP
--- rose_operations/src/grab/grab.cpp
+++ rose_operations/src/grab/grab.cpp
@@ -15,7 +15,7 @@
 Grab::Grab( std::string name, ros::NodeHandle n )
     : ManipulationBaseClass (name, n)
 {
-	smc_->addClient<rose_arm_controller_msgs::set_positionAction>("arm_controller/position");
+    smc_->addClient<rose_arm_controller_msgs::set_positionAction>("arm_controller/position");
     smc_->addClient<rose_arm_controller_msgs::set_gripper_widthAction>("arm_controller/gripper_width");
     smc_->addClient<rose_arm_controller_msgs::move_to_tfAction>("arm_visual_servoing");
 
@@ -24,7 +24,7 @@ Grab::Grab( std::string name, ros::NodeHandle n )
 
 Grab::~Grab()
 {
-	
+    
 }
 
 void Grab::CB_serverCancel( SMC* smc )
@@ -35,93 +35,93 @@ void Grab::CB_serverCancel( SMC* smc )
 // Goal received
 void Grab::executeItemAction( const std::string item_id, const vector<std::string> parameters )
 {
-	ROS_INFO("Grab::executeItemAction");
-	Item item = datamanager_->get<Item>(item_id);
+    ROS_INFO("Grab::executeItemAction");
+    Item item = datamanager_->get<Item>(item_id);
 
-	if (!item.get_bounding_box().isSet())
-	{
-		operator_gui_->action("Selecteer item");
-		getParameter(item_id, PARAMETER_REQUEST::BOUNDING_BOX, boost::bind(&Grab::continueItemAction, this, _1, _2));
-	}
-	else
-		executeGrabItem( item_id, parameters );
+    if (!item.get_bounding_box().isSet())
+    {
+        operator_gui_->action("Selecteer item");
+        getParameter(item_id, PARAMETER_REQUEST::BOUNDING_BOX, boost::bind(&Grab::continueItemAction, this, _1, _2));
+    }
+    else
+        executeGrabItem( item_id, parameters );
 }
 
 void Grab::continueItemAction( const std::string item_id, const vector<std::string> parameters )
 {
-	ROS_INFO("Grab::continueItemAction");
-	Item item = datamanager_->get<Item>(item_id);
-	if (!item.get_bounding_box().isSet())
-	{
-		result_.return_code = MISSING_PARAMETER_ERROR;
-		sendResult( false );
-	}
-	else
-		executeGrabItem ( item_id, parameters );
+    ROS_INFO("Grab::continueItemAction");
+    Item item = datamanager_->get<Item>(item_id);
+    if (!item.get_bounding_box().isSet())
+    {
+        result_.return_code = MISSING_PARAMETER_ERROR;
+        sendResult( false );
+    }
+    else
+        executeGrabItem ( item_id, parameters );
 }
 
 // Handy poses
 
 bool Grab::getPreGrabPose( Item item, PoseStamped& pose )
 {
-	ROS_DEBUG_NAMED(ROS_NAME, "getPreGrabPose");
+    ROS_DEBUG_NAMED(ROS_NAME, "getPreGrabPose");
 
-	double pre_grab = std::max(MIN_PRE_GRAB_X, (PRE_GRAB_X + (item.get_bounding_box().getDepth() / 2)));
+    double pre_grab = std::max(MIN_PRE_GRAB_X, (PRE_GRAB_X + (item.get_bounding_box().getDepth() / 2)));
 
     // ROS_DEBUG_NAMED(ROS_NAME, "getPreGrabPose. pre_grab =       = std::max(MIN_PRE_GRAB_X, (PRE_GRAB_X + (item.get_bounding_box().getDepth() / 2)));");
     // ROS_DEBUG_NAMED(ROS_NAME, "getPreGrabPose. pre_grab = %2.3f = std::max(%2.12f, (%2.8f + (%2.8f / 2)));",
     //                 pre_grab, MIN_PRE_GRAB_X, PRE_GRAB_X, item.get_bounding_box().getDepth());
 
-	if ( (item.get_bounding_box().getHeigth() / 2) < MIN_GRIPPER_TABLE_DIST_DOWN )
-    	return getPoseRelativeToGrab( item, pose, - pre_grab, 0.0, TABLE_CORRECTION );
-	else
-    	return getPoseRelativeToGrab( item, pose, - pre_grab, 0.0, TABLE_CORRECTION );
+    if ( (item.get_bounding_box().getHeigth() / 2) < MIN_GRIPPER_TABLE_DIST_DOWN )
+        return getPoseRelativeToGrab( item, pose, - pre_grab, 0.0, TABLE_CORRECTION );
+    else
+        return getPoseRelativeToGrab( item, pose, - pre_grab, 0.0, TABLE_CORRECTION );
 }
 
 bool Grab::getGrabPose( Item item, PoseStamped& pose )
 {
-	ROS_DEBUG_NAMED(ROS_NAME, "getGrabPose");
-	return getPoseRelativeToGrab( item, pose, 0.0, 0.0, 0.0 ); 
+    ROS_DEBUG_NAMED(ROS_NAME, "getGrabPose");
+    return getPoseRelativeToGrab( item, pose, 0.0, 0.0, 0.0 ); 
 }
 
 bool Grab::getPostGrabPose( Item item, PoseStamped& pose )
 {
-	ROS_DEBUG_NAMED(ROS_NAME, "getPostPose");
-	return getPoseRelativeToGrab( item, pose, 0.0, 0.0, POST_GRAB_Z );
+    ROS_DEBUG_NAMED(ROS_NAME, "getPostPose");
+    return getPoseRelativeToGrab( item, pose, 0.0, 0.0, POST_GRAB_Z );
 }
 
 bool Grab::getPoseRelativeToGrab( Item item, PoseStamped& pose, const double x, const double y, const double z )
 {
-	std::string item_id = item.get_id();
+    std::string item_id = item.get_id();
 
-	if ( not rose_transformations::getFrameInFrame(tf_, item_id, "base_link", pose))
+    if ( not rose_transformations::getFrameInFrame(tf_, item_id, "base_link", pose))
     {
         ROS_ERROR_NAMED(ROS_NAME, "Could not getFrameInFrame for %s to base_link", item_id.c_str());
-		return false;
+        return false;
     }
 
-	if ( not rose_transformations::addXYZInFrame(tf_, "base_link", x, y, z, pose))
+    if ( not rose_transformations::addXYZInFrame(tf_, "base_link", x, y, z, pose))
     {
         ROS_ERROR_NAMED(ROS_NAME, "Could not addXYZInFrame(/base_link, %2.3f, %2.3f, %2.3f) for frame %s", x, y, z, pose.header.frame_id.c_str());
-		return false;
+        return false;
     }
 
-	return true;
+    return true;
 }
 
 bool Grab::preGrab( const std::string arm_name, Item item )
 {
-	ROS_DEBUG_NAMED(ROS_NAME, "Pre grab");
+    ROS_DEBUG_NAMED(ROS_NAME, "Pre grab");
 
-	geometry_msgs::PoseStamped pre_grab;
+    geometry_msgs::PoseStamped pre_grab;
 
-	if ( not getPreGrabPose(item, pre_grab))
-		return false;
+    if ( not getPreGrabPose(item, pre_grab))
+        return false;
 
-	rose_arm_controller_msgs::set_positionGoal position_goal;
+    rose_arm_controller_msgs::set_positionGoal position_goal;
 
-	position_goal.arm 			= arm_name;
-	position_goal.required_pose = pre_grab;
+    position_goal.arm           = arm_name;
+    position_goal.required_pose = pre_grab;
 
     if ( not smc_->sendGoal<rose_arm_controller_msgs::set_positionAction>(position_goal, "arm_controller/position"))
     {
@@ -134,31 +134,31 @@ bool Grab::preGrab( const std::string arm_name, Item item )
     else 
         return true;
 
-	return true;
+    return true;
 }
 
 bool Grab::grab( const std::string arm_name, Item item )
 {
-	ROS_DEBUG_NAMED(ROS_NAME, "grab");
+    ROS_DEBUG_NAMED(ROS_NAME, "grab");
 
-	moveTowardsItem(arm_name, item);
+    moveTowardsItem(arm_name, item);
 
-	return true;
+    return true;
 }
 
 bool Grab::postGrab( const std::string arm_name, Item item )
 {
-	ROS_DEBUG_NAMED(ROS_NAME, "Post grab");
+    ROS_DEBUG_NAMED(ROS_NAME, "Post grab");
 
-	geometry_msgs::PoseStamped post_grab;
+    geometry_msgs::PoseStamped post_grab;
 
-	if ( not getPostGrabPose(item, post_grab))
-		return false;
+    if ( not getPostGrabPose(item, post_grab))
+        return false;
 
-	rose_arm_controller_msgs::set_positionGoal position_goal;
+    rose_arm_controller_msgs::set_positionGoal position_goal;
 
-	position_goal.arm 			= arm_name;
-	position_goal.required_pose = post_grab;
+    position_goal.arm           = arm_name;
+    position_goal.required_pose = post_grab;
 
     if ( not smc_->sendGoal<rose_arm_controller_msgs::set_positionAction>(position_goal, "arm_controller/position"))
     {
@@ -171,27 +171,27 @@ bool Grab::postGrab( const std::string arm_name, Item item )
     else 
         return true;
 
-	return true;
+    return true;
 }
 
 //! @todo MdL: Test basic 'visual servoing'.
 //! Replaces grab
 bool Grab::moveTowardsItem( const std::string arm_name, Item item )
 {
-	std::string item_id = item.get_id();
+    std::string item_id = item.get_id();
 
-	ROS_DEBUG_NAMED(ROS_NAME, "Move towards grab");
-	operator_gui_->message("Beweging richting item");
+    ROS_DEBUG_NAMED(ROS_NAME, "Move towards grab");
+    operator_gui_->message("Beweging richting item");
 
-	// Visual servoing
-	rose_arm_controller_msgs::move_to_tfGoal servoing_goal;
+    // Visual servoing
+    rose_arm_controller_msgs::move_to_tfGoal servoing_goal;
 
-	servoing_goal.arm_name 			= arm_name;
-	servoing_goal.tf_name  			= item_id;
-	// First move in xy (arm frame)
-	servoing_goal.max_xy_error  = 0.018;
+    servoing_goal.arm_name          = arm_name;
+    servoing_goal.tf_name           = item_id;
+    // First move in xy (arm frame)
+    servoing_goal.max_xy_error  = 0.018;
 
-	if ( not smc_->sendGoal<rose_arm_controller_msgs::move_to_tfAction>(servoing_goal, "arm_controller/positionarm_visual_servoing"))
+    if ( not smc_->sendGoal<rose_arm_controller_msgs::move_to_tfAction>(servoing_goal, "arm_controller/positionarm_visual_servoing"))
     {
         ROS_ERROR("Could not send visual servoing");
         return false;
@@ -206,52 +206,52 @@ bool Grab::moveTowardsItem( const std::string arm_name, Item item )
 // Grab sequence
 void Grab::executeGrabItem( const std::string item_id, const vector<std::string> parameters )
 {
-	std::string arm_name = "mico";
+    std::string arm_name = "mico";
 
-	operator_gui_->message("Grabbing item");
+    operator_gui_->message("Grabbing item");
 
-	// Get item details
-	Item item 	   = datamanager_->get<Item>(item_id);
-	geometry_msgs::PoseStamped item_pose = item.get_bounding_box().getPoseStamped();
-	
-	createTransform(item_id, item_pose);
+    // Get item details
+    Item item      = datamanager_->get<Item>(item_id);
+    geometry_msgs::PoseStamped item_pose = item.get_bounding_box().getPoseStamped();
+    
+    createTransform(item_id, item_pose);
 
-	operator_gui_->message("Geting arm to first pose");
-	// Go to pre grab
-	if ( not preGrab(arm_name, item) )
-	{
-		ROS_ERROR("Could not pre grab");
-		grabFailed(item, "Pre grab was not possible");
-		return;
-	}
+    operator_gui_->message("Geting arm to first pose");
+    // Go to pre grab
+    if ( not preGrab(arm_name, item) )
+    {
+        ROS_ERROR("Could not pre grab");
+        grabFailed(item, "Pre grab was not possible");
+        return;
+    }
 
-	operator_gui_->message("Going to grab");
-	// Grab with visual servoing
-	if ( not grab(arm_name, item) )
-	{
-		ROS_ERROR("Grab failed");
-		grabFailed(item, "Moving to the item was not possible");
-		return;
-	}
+    operator_gui_->message("Going to grab");
+    // Grab with visual servoing
+    if ( not grab(arm_name, item) )
+    {
+        ROS_ERROR("Grab failed");
+        grabFailed(item, "Moving to the item was not possible");
+        return;
+    }
 
-	// operator_gui_->message("Moving arm upwards");
-	// Pre grab
-	// if ( not postGrab(arm_name, item) )
-	// {
-	// 	ROS_ERROR("Post grab failed");
-	// 	grabFailed(item, "Could not move gripper up");
-	// 	return;
-	// }
+    // operator_gui_->message("Moving arm upwards");
+    // Pre grab
+    // if ( not postGrab(arm_name, item) )
+    // {
+    //  ROS_ERROR("Post grab failed");
+    //  grabFailed(item, "Could not move gripper up");
+    //  return;
+    // }
 
-	//! @todo MdL [IMPL]: post grab.
-	operator_gui_->message("Please use the joystick to move the gripper up");
+    //! @todo MdL [IMPL]: post grab.
+    operator_gui_->message("Please use the joystick to move the gripper up");
 
-	grabSuccess(item);
+    grabSuccess(item);
 }
 
 void Grab::grabSuccess( Item item )
 {
-	sendResultAndCleanup(true, item, "Grabbed item");
+    sendResultAndCleanup(true, item, "Grabbed item");
 }
 
 void Grab::grabFailed( Item item, std::string message)
@@ -261,14 +261,14 @@ void Grab::grabFailed( Item item, std::string message)
 
 void Grab::cleanup( Item item )
 {
-	std::string item_id = item.get_id();
+    std::string item_id = item.get_id();
 
-//	removeBoundingBoxOfItemFromDatabase(item); //! @todo MdL: Temporary implementation, keeps the db clean
-	removeTransform(item_id);
+//  removeBoundingBoxOfItemFromDatabase(item); //! @todo MdL: Temporary implementation, keeps the db clean
+    removeTransform(item_id);
 }
 
 void Grab::sendResultAndCleanup( bool result, Item item, string message )
 {
     sendResult(result, message);
-	cleanup(item);
+    cleanup(item);
 }
